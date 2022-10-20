7a38bbfd8d82a61148a0fa3eb02e69837d87e6c0
irayspace <irayspacii@gmail.com>
2022-10-20 14:04:54 +0800

    refactor: mCameraHelper to UvcView

--- android/src/main/kotlin/co/kahero/flutter_uvc/FlutterUvcPlugin.kt
+++ android/src/main/kotlin/co/kahero/flutter_uvc/FlutterUvcPlugin.kt
@@ -11,9 +11,6 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.StandardMessageCodec
 
-import com.herohan.uvcapp.CameraHelper
-import com.herohan.uvcapp.ICameraHelper
-
 /** FlutterUvcPlugin */
 class FlutterUvcPlugin: FlutterPlugin, MethodCallHandler {
   /// The MethodChannel that will the communication between Flutter and native Android
@@ -23,8 +20,6 @@ class FlutterUvcPlugin: FlutterPlugin, MethodCallHandler {
   private lateinit var channel: MethodChannel
   private lateinit var context: Context
 
-  private var mCameraHelper: ICameraHelper? = null
-  private var mUsbDevice: UsbDevice? = null
   private var mUvcViewFactory: UvcViewFactory? = null
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
@@ -35,36 +30,28 @@ class FlutterUvcPlugin: FlutterPlugin, MethodCallHandler {
 
     mUvcViewFactory = UvcViewFactory(StandardMessageCodec.INSTANCE, channel)
     flutterPluginBinding.getPlatformViewRegistry().registerViewFactory("flutter_uvc_view", mUvcViewFactory)
-
-    // init ICameraHelper 
-    if (mCameraHelper == null) {
-      val cameraHelper = CameraHelper()
-      val firstDevice = cameraHelper.getDeviceList().first()
-      mUsbDevice = firstDevice
-      mCameraHelper = cameraHelper
-    }
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if (call.method == "getPlatformVersion") {
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
     } else if (call.method == "getDeviceList") {
-      val deviceList = mCameraHelper?.getDeviceList()
-      result.success(deviceList?.map { device -> serializeUsbDevice(device) })
+      // val deviceList = mCameraHelper?.getDeviceList()
+      // result.success(deviceList?.map { device -> serializeUsbDevice(device) })
     } else if (call.method == "takePicture") {
       val path = context.getExternalFilesDir(null)?.getParent()
       result.success(path)
     } else if (call.method == "getDevice") {
-      val usbDevice: UsbDevice? = mUsbDevice
-      result.success(if (usbDevice != null) serializeUsbDevice(usbDevice) else null)
+      // val usbDevice: UsbDevice? = mUsbDevice
+      // result.success(if (usbDevice != null) serializeUsbDevice(usbDevice) else null)
     } else if (call.method == "selectDevice") {
-      val deviceId = call.argument("deviceId") as Int?
-      if (deviceId != null) {
-        mUsbDevice = mCameraHelper?.getDeviceList()?.filter { device -> device.getDeviceId() == deviceId}?.single()
-        result.success(true)
-      } else {
-        result.success(false)
-      }
+      // val deviceId = call.argument("deviceId") as Int?
+      // if (deviceId != null) {
+      //   mUsbDevice = mCameraHelper?.getDeviceList()?.filter { device -> device.getDeviceId() == deviceId}?.single()
+      //   result.success(true)
+      // } else {
+      //   result.success(false)
+      // }
     } else {
       result.notImplemented()
     }
--- android/src/main/kotlin/co/kahero/flutter_uvc/UvcView.kt
+++ android/src/main/kotlin/co/kahero/flutter_uvc/UvcView.kt
@@ -4,21 +4,29 @@ import android.view.View
 import android.view.LayoutInflater
 import android.view.SurfaceHolder
 import android.content.Context
+import android.hardware.usb.UsbDevice
 
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.platform.PlatformView
 
 import com.serenegiant.widget.AspectRatioSurfaceView
 
-class UvcView: PlatformView, SurfaceHolder.Callback {
+import com.herohan.uvcapp.CameraHelper
+import com.herohan.uvcapp.ICameraHelper
+
+class UvcView: PlatformView, SurfaceHolder.Callback, ICameraHelper.StateCallback {
     private var mNativeView: View
     private var mCameraView: AspectRatioSurfaceView
+    private var mCameraHelper: ICameraHelper
 
     constructor(context: Context, channel: MethodChannel) {
         mNativeView = LayoutInflater.from(context).inflate(R.layout.camera_view, null, false)
         mCameraView = mNativeView.findViewById(R.id.surface_view)
         mCameraView.setAspectRatio(640, 480)
         mCameraView.getHolder().addCallback(this)
+
+        mCameraHelper = CameraHelper()
+        mCameraHelper.setStateCallback(this)
     }
 
     override fun getView(): View {
@@ -30,7 +38,7 @@ class UvcView: PlatformView, SurfaceHolder.Callback {
     }
 
     override fun surfaceCreated(surfaceHolder: SurfaceHolder) {
-
+        mCameraHelper.addSurface(surfaceHolder.getSurface(), false)
     }
 
     override fun surfaceChanged(surfaceHolder: SurfaceHolder, format: Int, width: Int, height: Int) {
@@ -38,6 +46,35 @@ class UvcView: PlatformView, SurfaceHolder.Callback {
     }
 
     override fun surfaceDestroyed(surfaceHolder: SurfaceHolder) {
+        mCameraHelper.removeSurface(surfaceHolder.getSurface())
+    }
 
+    override fun onAttach(device: UsbDevice) {
+        mCameraHelper.selectDevice(device)
+    }
+
+    override fun onDeviceOpen(device: UsbDevice, isFirstOpen: Boolean) {
+        mCameraHelper.openCamera()
+    }
+
+    override fun onCameraOpen(device: UsbDevice) {
+        mCameraHelper.startPreview()
+        mCameraHelper.addSurface(mCameraView.getHolder().getSurface(), false)
+    }
+
+    override fun onCameraClose(device: UsbDevice) {
+        mCameraHelper.removeSurface(mCameraView.getHolder().getSurface())
+    }
+
+    override fun onDeviceClose(device: UsbDevice) {
+        println("onDeviceClose")
+    }
+
+    override fun onDetach(device: UsbDevice) {
+        println("onDetach")
+    }
+
+    override fun onCancel(device: UsbDevice) {
+        println("onCancel")
     }
 }
