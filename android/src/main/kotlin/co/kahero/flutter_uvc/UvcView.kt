94c383d0acf5bc5d94331770ed0a9500b34c79fa
irayspace <irayspacii@gmail.com>
2022-10-20 10:55:12 +0800

    feat: SurfaceHolder.Callback

--- android/src/main/kotlin/co/kahero/flutter_uvc/UvcView.kt
+++ android/src/main/kotlin/co/kahero/flutter_uvc/UvcView.kt
@@ -2,6 +2,7 @@ package co.kahero.flutter_uvc
 
 import android.view.View
 import android.view.LayoutInflater
+import android.view.SurfaceHolder
 import android.content.Context
 
 import io.flutter.plugin.common.MethodChannel
@@ -9,13 +10,15 @@ import io.flutter.plugin.platform.PlatformView
 
 import com.serenegiant.widget.AspectRatioSurfaceView
 
-class UvcView: PlatformView {
+class UvcView: PlatformView, SurfaceHolder.Callback {
     private var mNativeView: View
     private var mCameraView: AspectRatioSurfaceView
 
     constructor(context: Context, channel: MethodChannel) {
         mNativeView = LayoutInflater.from(context).inflate(R.layout.camera_view, null, false)
         mCameraView = mNativeView.findViewById(R.id.surface_view)
+        mCameraView.setAspectRatio(640, 480)
+        mCameraView.getHolder().addCallback(this)
     }
 
     override fun getView(): View {
@@ -23,6 +26,18 @@ class UvcView: PlatformView {
     }
 
     override fun dispose() {
+
+    }
+
+    override fun surfaceCreated(surfaceHolder: SurfaceHolder) {
+
+    }
+
+    override fun surfaceChanged(surfaceHolder: SurfaceHolder, format: Int, width: Int, height: Int) {
+
+    }
+
+    override fun surfaceDestroyed(surfaceHolder: SurfaceHolder) {
 
     }
 }
