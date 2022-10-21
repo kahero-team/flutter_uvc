69ec1fcf71ec8069d5ea0f3a929d41ec8cee4358
irayspace <irayspacii@gmail.com>
2022-10-21 17:00:42 +0800

    feat: currently capturing a photo guard

--- lib/flutter_uvc.dart
+++ lib/flutter_uvc.dart
@@ -9,6 +9,8 @@ import 'uvc_exception.dart';
 class FlutterUvc {
   static const MethodChannel _channel = MethodChannel('flutter_uvc');
 
+  static bool _isCapturing = false;
+
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
@@ -25,10 +27,17 @@ class FlutterUvc {
   }
 
   static Future<File> takePicture() async {
+    if (_isCapturing) {
+      throw UvcException("ALREADY_CAPTURING", "Currently capturing a photo");
+    }
+
+    _isCapturing = true;
     final String? path = await _channel.invokeMethod("takePicture");
     if (path == null) {
       throw UvcException("INVALID_PATH", "Unable to find the picture needed");
     }
+    _isCapturing = false;
+
     return File(path);
   }
 
